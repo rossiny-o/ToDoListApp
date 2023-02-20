fc11ee53d6c513173b0381b5ab5e5a01713fc997
rossiny-o <rossinyostcyr@gmail.com>
2023-02-19 19:03:10 -0500

    dropdown fixing 3

--- src/App.js
+++ src/App.js
@@ -151,11 +151,32 @@ function ToDoItem({ id, text, onDelete, onEdit }) {
     setIsEditing(false);
   }
 
-  function Table(props) {
+  function Table() {
     const [selectedUser, setSelectedUser] = useState("");
     const [selectedStatus, setSelectedStatus] = useState("");
 
-    const { users, status } = props;
+
+    const users = [
+      { id: 1, name: "John Doe", email: "johndoe@example.com" },
+      { id: 2, name: "Jane Doe", email: "janedoe@example.com" },
+      { id: 3, name: "Jim Smith", email: "jimsmith@example.com" },
+      { id: 4, name: "Alice Johnson", email: "alicejohnson@example.com" },
+      { id: 5, name: "Bob Williams", email: "bobwilliams@example.com" },
+      { id: 6, name: "Carol Davis", email: "caroldavis@example.com" },
+      { id: 7, name: "David Brown", email: "davidbrown@example.com" },
+      { id: 8, name: "Emily Wilson", email: "emilywilson@example.com" },
+      { id: 9, name: "Frank Taylor", email: "franktaylor@example.com" },
+      { id: 10, name: "Grace Lee", email: "gracelee@example.com" },
+      { id: 11, name: "Henry Clark", email: "henryclark@example.com" },
+      { id: 12, name: "Isabel Martinez", email: "isabelmartinez@example.com" },
+    ];
+  
+    const status = [
+      { id: 1, state: "Stuck" },
+      { id: 2, state: "In Progress" },
+      { id: 3, state: "Done" },
+    ];
+    
 
     function handleChange(event) {
       setIsChecked(event.target.checked);
@@ -188,7 +209,7 @@ function ToDoItem({ id, text, onDelete, onEdit }) {
         <div>
           <label htmlFor="dropdown1">
             <select
-              value={selectedUser}
+               value={selectedUser}
               onChange={handleUserSelectChange}
               className="dropdown1"
             >
@@ -200,6 +221,8 @@ function ToDoItem({ id, text, onDelete, onEdit }) {
               ))}
             </select>
           </label>
+
+         
         </div>
         &nbsp;&nbsp;
         <div>
@@ -232,26 +255,7 @@ function ToDoItem({ id, text, onDelete, onEdit }) {
     );
   }
 
-  const users = [
-    { id: 1, name: "John Doe", email: "johndoe@example.com" },
-    { id: 2, name: "Jane Doe", email: "janedoe@example.com" },
-    { id: 3, name: "Jim Smith", email: "jimsmith@example.com" },
-    { id: 4, name: "Alice Johnson", email: "alicejohnson@example.com" },
-    { id: 5, name: "Bob Williams", email: "bobwilliams@example.com" },
-    { id: 6, name: "Carol Davis", email: "caroldavis@example.com" },
-    { id: 7, name: "David Brown", email: "davidbrown@example.com" },
-    { id: 8, name: "Emily Wilson", email: "emilywilson@example.com" },
-    { id: 9, name: "Frank Taylor", email: "franktaylor@example.com" },
-    { id: 10, name: "Grace Lee", email: "gracelee@example.com" },
-    { id: 11, name: "Henry Clark", email: "henryclark@example.com" },
-    { id: 12, name: "Isabel Martinez", email: "isabelmartinez@example.com" },
-  ];
-
-  const status = [
-    { id: 1, state: "Stuck" },
-    { id: 2, state: "In Progress" },
-    { id: 3, state: "Done" },
-  ];
+  
 
   //   useEffect(() => {
   //     console.table(users);
@@ -275,15 +279,79 @@ function ToDoItem({ id, text, onDelete, onEdit }) {
     );
   }
 
+  const test = [
+    { id: 1, name: "John Doe", email: "johndoe@example.com" },
+    { id: 2, name: "Jane Doe", email: "janedoe@example.com" },
+    { id: 3, name: "Jim Smith", email: "jimsmith@example.com" }
+  ];
+
   return (
     <div className="todo-tablegrid">
       <label htmlFor="checkbox" className={isChecked ? "todo-text-strike" : ""}>
-        <Table users={users} status={status} />
+        <Table />
       </label>
+
+      <Dropdown />
+
+      <label>
+            <select>
+              {test.map( user => (
+                <option key={user.id} value={user.name}>{user.name}</option>
+              ))}
+            </select>
+          </label>
     </div>
   );
 
   // test extra code
+}
+
+function Dropdown() {
+
+  const [selectedUser, setSelectedUser] = useState("");
+
+  function handleUserSelectChange(event) {
+    setSelectedUser(event.target.value);
+  }
+
+  const users = [
+    { id: 1, name: "John Doe", email: "johndoe@example.com" },
+    { id: 2, name: "Jane Doe", email: "janedoe@example.com" },
+    { id: 3, name: "Jim Smith", email: "jimsmith@example.com" },
+    { id: 4, name: "Alice Johnson", email: "alicejohnson@example.com" },
+    { id: 5, name: "Bob Williams", email: "bobwilliams@example.com" },
+    { id: 6, name: "Carol Davis", email: "caroldavis@example.com" },
+    { id: 7, name: "David Brown", email: "davidbrown@example.com" },
+    { id: 8, name: "Emily Wilson", email: "emilywilson@example.com" },
+    { id: 9, name: "Frank Taylor", email: "franktaylor@example.com" },
+    { id: 10, name: "Grace Lee", email: "gracelee@example.com" },
+    { id: 11, name: "Henry Clark", email: "henryclark@example.com" },
+    { id: 12, name: "Isabel Martinez", email: "isabelmartinez@example.com" },
+  ];
+
+  return (
+
+    <div>
+          <label htmlFor="dropdown1">
+            <select
+               value={selectedUser}
+              onChange={handleUserSelectChange}
+              className="dropdown1"
+            >
+              <option value="">Select user...</option>
+              {users.map((user) => (
+                <option key={user.id} value={user.name}>
+                  {user.name}
+                </option>
+              ))}
+            </select>
+          </label>
+
+         
+        </div>
+
+  );
+
 }
 
 export default ToDoList;
